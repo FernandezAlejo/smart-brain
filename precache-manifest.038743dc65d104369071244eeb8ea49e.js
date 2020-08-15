88709e4ec7b47b8f9a0ac4295268792f92d7846c
FernandezAlejo <Alejo.FernandezSaiz@gmail.com>
2020-08-14 21:23:16 -0300

    Updates

---
+++ precache-manifest.038743dc65d104369071244eeb8ea49e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9baf40cd5c07ba468bff102403d3877b",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "17f3a1127f34cea48336",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e097eddc75f956c7226",
+    "url": "/facerecognitionbrain/static/css/main.b24d7012.chunk.css"
+  },
+  {
+    "revision": "17f3a1127f34cea48336",
+    "url": "/facerecognitionbrain/static/js/2.81cb10c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionbrain/static/js/2.81cb10c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e097eddc75f956c7226",
+    "url": "/facerecognitionbrain/static/js/main.5ce194ea.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
